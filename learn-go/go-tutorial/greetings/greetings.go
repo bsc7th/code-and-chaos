d8b32aae8117209fe96a57ccd4757cf0fbd0d4aa
bsc7th <ryanjayleyva@proton.me>
2025-04-05 11:32:22 -0400

    fix typo "string"

--- learn-go/go-tutorial/greetings/greetings.go
+++ learn-go/go-tutorial/greetings/greetings.go
@@ -3,7 +3,7 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name srting) string {
+func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprinttf("Hi, %v. Welcome!", name)
 	return message
